da71e4284c97b7e955bf2dcc631eb8e2eb7d0796
Digas-2 <diogo.a.f.fernandes@gmail.com>
2020-01-01 16:57:42 +0000

    Added seeder closure and new json format type

--- SD_grupoA/src/main/java/com/example/sdJav/SD_Server.java
+++ SD_grupoA/src/main/java/com/example/sdJav/SD_Server.java
@@ -1,11 +1,13 @@
 package com.example.sdJav;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 import java.util.Collection;
+import java.util.List;
 
 
 public class SD_Server {
@@ -13,12 +15,13 @@ public class SD_Server {
     private io.grpc.Server server;
 
     private class GreeterImpl extends GreeterGrpc.GreeterImplBase {
-        private final Collection<Seeder> seeders_list = null;
-        private final JsonArray seeders = new JsonArray();
+        private final List<Seeder> seeders_list = null;
+        //private final JsonArray seeders = new JsonArray();
+        private String jsonSeederObject = new String();
 
         private boolean checkSeeder(Seeder seeder) {
             for (Seeder s : seeders_list) {
-                if (s.getStreamName() == seeder.getStreamName()) {
+                if (s.getStreamName().equals(seeder.getStreamName())) {
                     return true;
                 }
             }
@@ -33,21 +36,28 @@ public class SD_Server {
             else {
                 Seeder.newBuilder().setStreamName(request.getStreamName()).setBitrate(request.getBitrate()).setEndPoint(request.getEndPoint()).build();
                 response = GetSeederResponse.newBuilder().setMessage("Seeder ID " + request.getStreamName() + " registry successful!").build();
-                JsonObject object = new JsonObject();
-                object.addProperty("Stream_Name", request.getStreamName());
-                object.addProperty("Bitrate", request.getBitrate());
-                object.addProperty("Video_Size", request.getVideoSize());
-                this.seeders.add(object);
+
+                Gson gson = new Gson();
                 this.seeders_list.add(request);
+                jsonSeederObject = gson.toJson(this.seeders_list);
             }
             responseStreamObserver.onNext(response);
             responseStreamObserver.onCompleted();
         }
 
         public void closureSeeder(Seeder request, StreamObserver<GetSeederResponse> responseStreamObserver) {
-            if (checkSeeder(request)) {
+            GetSeederResponse response;
+            if(checkSeeder(request)){
                 this.seeders_list.remove(request);
+                Gson gson = new Gson();
+                jsonSeederObject = gson.toJson(this.seeders_list);
+                response = GetSeederResponse.newBuilder().setMessage(request.getStreamName()+ "closed").build();
             }
+            else {
+                response = GetSeederResponse.newBuilder().setMessage(request.getStreamName()+ "inexistant").build();
+            }
+            responseStreamObserver.onNext(response);
+            responseStreamObserver.onCompleted();
         }
     }
 
